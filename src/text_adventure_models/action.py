4de8908d5074beee4c4dfef9f3ecb5979ddcbd30
Iain Brown <codeiain@outlook.com>
2022-08-28 14:14:11 +0000

    started building models

--- src/text_adventure_models/action.py
+++ src/text_adventure_models/action.py
@@ -1,5 +1,6 @@
 class Action(object):
-    def __init__(self,action, target, description):
+    def __init__(self, action, target, description, status=None):
         self.action = action
         self.target = target
         self.description = description
+        self.status = status
--- src/text_adventure_models/room_exit.py
+++ src/text_adventure_models/room_exit.py
@@ -1,5 +1,7 @@
 class RoomExit(object):
-    def __init__(self, direction, description, linksTo=""):
+    def __init__(self, direction, description, status=None, linksTo="", condition=None):
         self.direction = direction
         self.description = description
+        self.status = status
         self.linksTo = linksTo
+        self.condition = condition
